ee7bceceb982f55f200596d08f49def77a669a1e
s23739-mh <73026549+s23739-mh@users.noreply.github.com>
2020-11-08 20:36:46 +0200

    Update s03-fizzbuzz.cpp

--- src/s03-fizzbuzz.cpp
+++ src/s03-fizzbuzz.cpp
@@ -6,14 +6,15 @@ std::cout<<"Enter a number:";
 std::getline(std::cin,a);
 for(int n=0;n<=std::stoi(a);n++)
 {
+std::cout<<n<<" ";
 if(n%(3*5)==0)
-{std::cout<<"Fizzbuzz\n";}
+{std::cout<<"Fizzbuzz ";}
 else
 {
 if(n%3==0)
-{std::cout<<"Fizz\n";}
+{std::cout<<"Fizz ";}
 if(n%5==0)
-{std::cout<<"Buzz\n";}
+{std::cout<<"Buzz ";}
 }
 }
 
