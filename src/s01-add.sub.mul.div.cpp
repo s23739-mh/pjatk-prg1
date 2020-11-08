de206270931f8d16eb928a9072ecb77644bde405
s23739-mh <s23739@pjwstk.edu.pl>
2020-11-08 16:34:30 +0100

    Arrears

---
+++ src/s01-add.sub.mul.div.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+auto ask_user_for_integer(std::string prompt) ->float
+{
+std::cout<<prompt;
+auto n = std::string{};
+std::getline(std::cin,n);
+return std::stof(n);
+}
+auto main () ->int
+{
+auto const a = ask_user_for_integer("a= ");
+auto const b = ask_user_for_integer("b= ");
+
+std::cout<<"a+b="<<a+b<<"\n";
+std::cout<<"a-b="<<a-b<<"\n";
+std::cout<<"a*b="<<a*b<<"\n";
+std::cout<<"a/b="<<a/b<<"\n";
+return 0;
+}
