ac331c3c87620f737f14766d620b0b05a4a7730e
s23739-mh <s23739@pjwstk.edu.pl>
2021-02-05 14:11:26 +0100

    ...

--- src/s05-sleeping-threads-2.cpp
+++ src/s05-sleeping-threads-2.cpp
@@ -6,24 +6,27 @@
 #include <string>
 #include <thread>
 #include <unistd.h>
-auto fill_queue(std::queue<std::string> &text) -> void {
+auto fill_queue(std::queue<std::string> &text, std::mutex &mtx,
+                std::condition_variable &cv) -> void {
   std::string help = " ";
   auto i = 0;
   while (i < 4) {
     std::getline(std::cin, help);
+    std::unique_lock<std::mutex> locker(mtx);
     if (help == " " || help[0] == 0 || help[0] == 10) {
       i++;
     }
     text.push(help);
+    mtx.unlock();
+    cv.notify_one();
   }
+  return;
 }
 
 auto get_from_queue(std::mutex &mtx, std::queue<std::string> &q, int thread_ID,
                     std::condition_variable &cv) -> void {
   std::unique_lock<std::mutex> locker(mtx);
-  // std::cout<<"heelp"; - I dont have a debugger
   cv.wait(locker);
-
   if (q.empty() == false) {
     if (q.front()[0] != 0 && q.front()[0] != 10) {
       std::cout << "from thread" << thread_ID << ": " << q.front() << "\n";
@@ -31,14 +34,17 @@ auto get_from_queue(std::mutex &mtx, std::queue<std::string> &q, int thread_ID,
       std::cout << "thread " << thread_ID << " exiting\n";
     }
     q.pop();
+    cv.notify_one();
   }
+
+  return;
 }
 
 auto main() -> int {
   std::queue<std::string> q;
-  fill_queue(q);
   std::mutex mtx;
   std::condition_variable cv;
+  fill_queue(std::ref(q), std::ref(mtx), std::ref(cv));
   while (true) {
 
     auto thread1 = std::thread{get_from_queue, std::ref(mtx), std::ref(q), 401,
@@ -49,7 +55,6 @@ auto main() -> int {
                                std::ref(cv)};
     auto thread4 = std::thread{get_from_queue, std::ref(mtx), std::ref(q), 404,
                                std::ref(cv)};
-    cv.notify_one();
     thread1.join();
     thread2.join();
     thread3.join();
